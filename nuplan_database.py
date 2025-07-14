906b2dc584311c283e34269fe29daa50960bb010
Yizhou Wang <wangyizhou3@gmail.com>
2025-07-13 22:43:11 -0700

    minor changes

--- nuplan_database.py
+++ nuplan_database.py
@@ -1,10 +1,10 @@
 import os
 from nuplan.database.nuplan_db_orm.nuplandb_wrapper import NuPlanDBWrapper
 
-NUPLAN_DATA_ROOT = os.getenv('NUPLAN_DATA_ROOT', './')
-NUPLAN_MAPS_ROOT = os.getenv('NUPLAN_MAPS_ROOT', './nuplan-maps-v1.0')
-NUPLAN_DB_FILES = os.getenv('NUPLAN_DB_FILES', './nuplan-v1.1/splits/mini')
-NUPLAN_MAP_VERSION = os.getenv('NUPLAN_MAP_VERSION', 'nuplan-maps-v1.0')
+NUPLAN_DATA_ROOT = os.getenv("NUPLAN_DATA_ROOT", "./nuplan-v1.1/")
+NUPLAN_MAPS_ROOT = os.getenv("NUPLAN_MAPS_ROOT", "./nuplan-maps-v1.0")
+NUPLAN_DB_FILES = os.getenv("NUPLAN_DB_FILES", "./nuplan-v1.1/splits/mini")
+NUPLAN_MAP_VERSION = os.getenv("NUPLAN_MAP_VERSION", "nuplan-maps-v1.0")
 
 
 nuplandb_wrapper = NuPlanDBWrapper(
@@ -19,3 +19,28 @@ log_db = nuplandb_wrapper.get_log_db(log_db_name)
 
 lidar_pcs = log_db.lidar_pc
 print(f"The number of lidar_pcs in this log file is {len(lidar_pcs)}.")
+
+example_lidar_pc = lidar_pcs[0]
+example_ego_pose = example_lidar_pc.ego_pose
+print(f"Lidar_pc token: {example_lidar_pc.token}.")
+print(f"Ego pose: <{example_ego_pose.x}, {example_ego_pose.y}, {example_ego_pose.z}>.")
+
+
+print(f"Lidar boxes: {len(example_lidar_pc.lidar_boxes)}.")
+# print(f"Lidar box 0 : {example_lidar_pc.lidar_boxes[0]}.")
+# print(f"Lidar box 0 : {example_lidar_pc.boxes()[0]}.")
+
+render = False
+if render:
+    import matplotlib.pyplot as plt
+
+    example_lidar_pc.render(
+        log_db,
+        render_future_waypoints=True,
+        render_map_raster=True,
+        render_vector_map=True,
+        render_track_color=True,
+        render_future_ego_poses=True,
+    ).plot()
+
+    plt.show()
